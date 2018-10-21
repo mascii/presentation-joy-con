69688c023dcdd150b87464a92284271a123986c4
Masaki Koyanagi <mascii@gmail.com>
2018-10-21 15:54:36 +0900

    tweak

--- src/presentation-joy-con.js
+++ src/presentation-joy-con.js
@@ -1,32 +1,32 @@
 (() => {
     const [leftButton, rightButton] = [0, 3];
-    const [leftArrowKey, rightArrowKey] = [37, 39];
+    const [leftArrowKeyCode, rightArrowKeyCode] = [37, 39];
 
     let isPressing = false;
-    const keyPress = keyCode => {
+    const pressKey = keyCode => {
         if (isPressing) {
             return;
         }
 
         const activeElement = document.activeElement;
         const global = document.activeElement.tagName === 'IFRAME' ? activeElement.contentWindow : window;
-        [global, global.document].forEach(target => {
-            ['keyup', 'keydown'].forEach(eventName => {
-                target.dispatchEvent(new KeyboardEvent(eventName, { keyCode }));
+        ['keydown', 'keyup'].forEach(typeArg => {
+            [global.document, global].forEach(target => {
+                target.dispatchEvent(new KeyboardEvent(typeArg, { keyCode }));
             });
         });
 
         isPressing = true;
     };
     setInterval(() => {
-        const gamepad = navigator.getGamepads()[0]
+        const gamepad = navigator.getGamepads()[0];
         if (gamepad) {
             const buttons = gamepad.buttons;
             if (buttons[leftButton].pressed) {
-                keyPress(leftArrowKey);
+                pressKey(leftArrowKeyCode);
                 return;
             } else if (buttons[rightButton].pressed) {
-                keyPress(rightArrowKey);
+                pressKey(rightArrowKeyCode);
                 return;
             }
         }
